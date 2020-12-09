75cae84327c0cb7e55640de8b162a4f1a1951776
zhangkun <zhangkun35@maoyan.com>
2020-12-09 09:01:38 +0800

    提交代码

--- app/src/main/java/com/mao/coding/MainActivity.java
+++ app/src/main/java/com/mao/coding/MainActivity.java
@@ -26,9 +26,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-//        navController.handleDeepLink(getIntent());
         NavGraphBuilder.build(this, navController, 1);
+
+        // deepLink 隐式意图跳转
+        //navController.handleDeepLink(getIntent());
     }
 
 }
